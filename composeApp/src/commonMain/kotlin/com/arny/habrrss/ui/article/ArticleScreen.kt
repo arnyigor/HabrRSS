c5b6f5c4559f1643dfbf9f823f758fb24113f0f8
Arnyigor <arnyigor@gmail.com>
2026-08-14 15:14:00 +0500

    fix

--- composeApp/src/commonMain/kotlin/com/arny/habrrss/ui/article/ArticleScreen.kt
+++ composeApp/src/commonMain/kotlin/com/arny/habrrss/ui/article/ArticleScreen.kt
@@ -1,6 +1,7 @@
 package com.arny.habrrss.ui.article
 
 
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
